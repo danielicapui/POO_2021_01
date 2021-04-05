4f95f889399bad939d4815731e25b85d70f60df1
danielicapui <37754709+danielicapui@users.noreply.github.com>
2021-04-05 06:58:22 -0300

    Algumas modificações

--- GUI.java
+++ GUI.java
@@ -1,24 +1,23 @@
 package deck.carta.Jogador;
-import javax.swing.*;
-class Gui
+import javax.swing.JFrame;
+class GUI
 {
   private JButton[] b1=new JButton[5];
   private JButton[] b2=new JButton[5];
-	private Jogador j1;
+  private Jogador j1;
   private Jogador j2;
-
-  Gui(Jogador j1,Jogador j2)
+  private JFrame jogo;
+  GUI(Jogador j1,Jogador j2)
   {
   	this.j1=j1;
   	this.j2=j2;
-    
-
+    int tamanho=50;
     for(int i=0;i<5;i++)
     {
-    	//Icon carta1=j1.carta
+      //Icon carta1=j1.carta
       //Icon carta2=new ImageIcon(
-      this.b1[i]=j1.getMao[i];
-      this.b2[i]=j2.getMao[i];
+      this.b1[i]=new JButton(j1.getMao[i]().getImagem());
+      this.b2[i]=new JButton(j2.getMao[i]().getImagem());
     }
   }
  	
